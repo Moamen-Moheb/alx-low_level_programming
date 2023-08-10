4c6f89a393e6df1030d8718a36d0db7ab4549fca
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-10 15:25:40 -0700

    asdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'print using puts'
+ * @parameter: no parameters here  
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
