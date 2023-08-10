900391ee06a7158fe8fd72dc44f19b90de03c801
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-10 15:33:52 -0700

    asdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,7 @@
 
 /**
  * main - Entry point
- * 
- * Description: 'the program's description'
- * @parameter: describe the parameter
- * 
+ * Description: 'the program's description' 
  * Return: Always 0 (Success)
  */
 
