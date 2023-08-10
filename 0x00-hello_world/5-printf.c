cac335e100d487566a25a0dceb75632a46d261e1
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-10 15:53:17 -0700

    asdf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-  *
-  * main - Here we go 
-  * Return: Always 0 (Success)
-  */
+ * main - Here we go
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
