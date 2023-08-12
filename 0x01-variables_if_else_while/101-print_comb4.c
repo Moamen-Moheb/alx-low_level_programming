def5ad151561ca137d20357102bfa4590a040ea0
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-11 19:06:14 -0700

    asdfasdf

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  */
 int main()
 {
-    char digit[4];
+    int digit[2];
     int i;
     int j;
     int k;
@@ -17,11 +17,10 @@ int main()
         {
             for (k = j + 1; k < 10; k++)
             {
-                digit[0] = i + '0';
-                digit[1] = j + '0';
-                digit[2] = k + '0';
-                digit[3] = '\0';
-                printf("%s\n", digit);
+                digit[0] = i;
+                digit[1] = j;
+                digit[2] = k;
+                printf("%d%d%d\n", digit[0],digit[1],digit[2]);
             }
         }
     }
