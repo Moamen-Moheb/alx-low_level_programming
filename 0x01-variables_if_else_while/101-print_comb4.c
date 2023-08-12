3500ac7caa3a77a923c45b3a09ffb7e055fa1abb
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-11 18:51:08 -0700

    asdfasdf

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#incdlue<stdio.h>
+\*
+ * main - this is main function.
+ *
+ * Return: 0 on success
+int main()
+{
+	char digit[4];
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = i + 1; j < 9; j++)
+		{
+			for (int k = j + 1; k < 10; k++)
+			{
+				digit[0] = i + '0';
+				digit[1] = j + '0';
+				digit[2] = k + '0';
+				digit[3] = '\0';
+				printf("%s\n", digit);
+			}
+		}
+	}
+	return 0;
+}
