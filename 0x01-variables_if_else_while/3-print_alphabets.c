287eb76bac422af6361e42f9beb656f6b72f52ab
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-11 18:28:09 -0700

    asdfasdf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabets from a to z.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <='Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
