d0f08bbe282ca521e78595fa3534407460c1b9eb
Moamen-Moheb <moamen_moheb1710@student.aast.edu>
2023-08-11 19:01:56 -0700

    asdfasdf

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,30 @@
-#include<stdio.h>
-/*
- * main - this is main function.
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
  *
  * Return: 0 on success
-*/
+ */
 int main()
 {
-	char digit[4];
+    char digit[4];
+    int i;
+    int j;
+    int k;
 
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = i + 1; j < 9; j++)
-		{
-			for (int k = j + 1; k < 10; k++)
-			{
-				digit[0] = i + '0';
-				digit[1] = j + '0';
-				digit[2] = k + '0';
-				digit[3] = '\0';
-				printf("%s\n", digit);
-			}
-		}
-	}
-	return 0;
+    for (i = 0; i < 8; i++)
+    {
+        for (j = i + 1; j < 9; j++)
+        {
+            for (k = j + 1; k < 10; k++)
+            {
+                digit[0] = i + '0';
+                digit[1] = j + '0';
+                digit[2] = k + '0';
+                digit[3] = '\0';
+                printf("%s\n", digit);
+            }
+        }
+    }
+
+    return 0;
 }
